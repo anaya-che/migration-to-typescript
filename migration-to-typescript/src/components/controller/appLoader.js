51eea1bfb2a77ace8e09dcaf2a69d1c6663e33d8
anaya-che <anaya.che22@gmail.com>
2021-12-04 17:51:49 +0300

    refactor: apply prettier

--- migration-to-typescript/src/components/controller/appLoader.js
+++ migration-to-typescript/src/components/controller/appLoader.js
@@ -1,11 +1,11 @@
 import Loader from './loader';
 
 class AppLoader extends Loader {
-    constructor() {
-        super('https://newsapi.org/v2/', {
-            apiKey: '1e28876a4853438a9a9cfbbe3e23070b',
-        });
-    }
+  constructor() {
+    super('https://newsapi.org/v2/', {
+      apiKey: '1e28876a4853438a9a9cfbbe3e23070b',
+    });
+  }
 }
 
 export default AppLoader;
--- migration-to-typescript/webpack.config.js
+++ migration-to-typescript/webpack.config.js
@@ -2,42 +2,44 @@ const path = require('path');
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpackProdConfig = require('./webpack.prod.config');
+const webpackDevConfig = require('./webpack.dev.config');
 
 const baseConfig = {
-    entry: path.resolve(__dirname, './src/index.ts'),
-    mode: 'development',
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-              test: /\.tsx?$/,
-              use: 'ts-loader',
-              exclude: /node_modules/,
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    output: {
-        filename: 'index.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, './src/index.html'),
-            filename: 'index.html',
-        }),
-        new CleanWebpackPlugin(),
+  entry: path.resolve(__dirname, './src/index.ts'),
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
     ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+      filename: 'index.html',
+    }),
+    new CleanWebpackPlugin(),
+  ],
 };
 
 module.exports = ({ mode }) => {
-    const isProductionMode = mode === 'prod';
-    const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
+  const isProductionMode = mode === 'prod';
+  const envConfig = isProductionMode ? webpackProdConfig : webpackDevConfig;
 
-    return merge(baseConfig, envConfig);
+  return merge(baseConfig, envConfig);
 };
