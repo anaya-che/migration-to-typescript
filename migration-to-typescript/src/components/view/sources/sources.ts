3bf0c3ba93450a19dbd6d35ab3cf8c92da76ac0d
anaya-che <anaya.che22@gmail.com>
2021-12-04 19:19:57 +0300

    feat: change sources from JavaScript to TypeScript

---
+++ migration-to-typescript/src/components/view/sources/sources.ts
@@ -0,0 +1,29 @@
+import './sources.css';
+
+interface ISources {
+  id: string;
+  name: string;
+}
+
+class Sources {
+  draw(data: ISources[]): void {
+    const fragment: DocumentFragment = document.createDocumentFragment();
+    const sourceItemTemp = <HTMLTemplateElement>document.querySelector('#sourceItemTemp');
+    const sourcesElement = <HTMLElement>document.querySelector('.sources');
+
+    data.forEach((item: ISources): void => {
+      const sourceClone = <HTMLTemplateElement>sourceItemTemp.content.cloneNode(true);
+      const sourceItemName = <HTMLElement>sourceClone.querySelector('.source__item-name');
+      const sourceItem = <HTMLElement>sourceClone.querySelector('.source__item');
+
+      sourceItemName.textContent = item.name;
+      sourceItem.setAttribute('data-source-id', item.id);
+
+      fragment.append(sourceClone);
+    });
+
+    sourcesElement.append(fragment);
+  }
+}
+
+export default Sources;
