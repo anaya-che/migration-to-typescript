f83dbf40c07f3ed8cd5ebddb808bc91fe0e0e580
anaya-che <anaya.che22@gmail.com>
2021-12-06 19:08:09 +0300

    feat: change link to news api

--- migration-to-typescript/src/components/controller/appLoader.ts
+++ migration-to-typescript/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
   constructor() {
-    super('https://newsapi.org/v2/', {
+    super('https://nodenews.herokuapp.com/', {
       apiKey: '1e28876a4853438a9a9cfbbe3e23070b',
     });
   }
